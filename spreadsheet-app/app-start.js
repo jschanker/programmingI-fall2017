992af8db26f5dc5855f2c101240725337c4cfdac
jschanker <jschanker@molloy.edu>
2017-06-12 04:10:28 -0400

    Updates to Spreadsheet App
    1. Removed duplication of spreadsheet app heading
    2. Better modularization.
    3. Fixed selection; input cell blurs when other cells are selected.
    4. Data now stays the same when cell format changes (time, currency, or number).
    5. All cells deselect when a cell is clicked.

--- spreadsheet-app/app-start.js
+++ spreadsheet-app/app-start.js
@@ -1,12 +1,56 @@
 //alert(document.querySelectorAll(".input-cell"));
 var selectedCell = null;
-var changeDataVal = true;
 
 function select(event) {
-  if(event.which == 1) {
-    event.target.classList.add("selected");
+  if(mouseButtonPressed(event, 1)) {
+    selectCell(event.target);
     event.preventDefault();
   }
+}
+
+function showContextMenu(x, y) {
+  document.getElementById("context-menu").classList.add("context-menu-display");
+  document.getElementById("context-menu").classList.remove("hidden");
+
+  if(typeof x === "number" && typeof y === "number") {
+    document.getElementById("context-menu").style.left = x + "px";
+    document.getElementById("context-menu").style.top = y + "px";
+  }
+}
+
+function hideContextMenu() {
+  document.getElementById("context-menu").classList.remove("context-menu-display");
+  document.getElementById("context-menu").classList.add("hidden");
+}
+
+function actOnAllCells(action) {
+  Array.prototype.forEach.call(document.querySelectorAll(".input-cell"), action);
+}
+
+function deselectAllCells() {
+  actOnAllCells(function(cell) {
+    cell.classList.remove("selected");
+  });
+}
+
+function blurAllCells() {
+  actOnAllCells(function(cell) {
+    cell.blur();
+  });
+}
+
+function selectCell(cell) {
+  cell.classList.add("selected");
+}
+
+function mouseButtonPressed(event, index) {
+  // See browser support: http://eloquentjavascript.net/14_event.html#p_fndkFYbayW
+  //                    : https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
+  //                    : https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/which
+
+  var buttonClickedIndex = event.buttons !== null ? event.buttons : event.which;
+
+  return buttonClickedIndex === index;
 }
 
 function evalCell(cell) {
@@ -24,44 +68,59 @@ function evalCell(cell) {
 
 Array.prototype.forEach.call(document.querySelectorAll(".input-cell"),function(ele){ 
   ele.setAttribute("data-value", "");
+  //ele.setAttribute("display-value", "");
   
   ele.addEventListener("mouseover", select);
+
   ele.addEventListener("blur", function(event) {
-    if(changeDataVal) {
+    displayValue = evalCell(event.target);
+    //alert(displayValue + " " + event.target.value);
+    if(displayValue != event.target.value) {
+      // cell value changed
       event.target.setAttribute("data-value", event.target.value);
-    } else {
-      changeDataVal = true;
     }
   });
+
   ele.addEventListener("click", function(event){
-    event.target.classList.remove("selected");
-    document.getElementById("context-menu").classList.remove("context-menu-display");
-    document.getElementById("context-menu").classList.add("hidden");
+    deselectAllCells();
+    hideContextMenu();
    });
+
+  ele.addEventListener("focus", function(event){
+    //deselectAllCells();
+    hideContextMenu();
+  });
+
   ele.addEventListener("contextmenu", function(event) {
     event.preventDefault();
     selectedCell = event.target;
-    document.getElementById("context-menu").classList.add("context-menu-display");
-    document.getElementById("context-menu").classList.remove("hidden");
-    document.getElementById("context-menu").style.left = event.pageX +"px";
-    document.getElementById("context-menu").style.top = event.pageY +"px";
+    showContextMenu(event.pageX, event.pageY);
   });
+
   ele.addEventListener("mousemove", function(event) {
-    if(event.which == 1) {
-        event.target.classList.add("selected");
+    if(mouseButtonPressed(event, 1)) {
+      //alert(event.target.id);
+      //event.target.blur();
+      if(document.activeElement !== event.target) {
+        //alert("Blur");
+        //blurAllCells();
+        setTimeout(blurAllCells, 100);
+      }
+      selectCell(event.target);
     }
   });
 });
 
 Array.prototype.forEach.call(document.querySelectorAll(".format"),function(ele){
   ele.addEventListener("click", function(event) {
-    document.getElementById("context-menu").classList.remove("context-menu-display");
-    document.getElementById("context-menu").classList.add("hidden");
+    hideContextMenu();
     //alert(event.target.getAttribute("data-value"));
     if(selectedCell) {
       selectedCell.setAttribute("data-format", event.target.getAttribute("data-type"));
-      changeDataVal = false;
     }
+
+    //selectedCell.setAttribute("display-value", evalCell(selectedCell)); 
+    //selectedCell.value = selectedCell.getAttribute("display-value");
     selectedCell.value = evalCell(selectedCell);
   });
 });
